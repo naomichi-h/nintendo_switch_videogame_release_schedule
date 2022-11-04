75941b7e7715cdf2d6eed8832d00e0db74ec1097
naomichi-h <naomichi.programming@gmail.com>
2022-11-04 20:28:12 +0900

    first commit

---
+++ lib/nintendo_switch_videogame_release_schedule.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "nintendo_switch_videogame_release_schedule/version"
+
+module NintendoSwitchVideogameReleaseSchedule
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/nintendo_switch_videogame_release_schedule_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe NintendoSwitchVideogameReleaseSchedule do
+  it "has a version number" do
+    expect(NintendoSwitchVideogameReleaseSchedule::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
